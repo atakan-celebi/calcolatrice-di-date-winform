45fb2b51fab0736cdf579919d1a9aeb5b60185ed
Atakan Celebi <atakancelebi2007@gmail.com>
2025-04-22 16:12:44 +0200

    Add project files.

---
+++ Cakcikatire du date winform/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Cakcikatire_du_date_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DTP1 = new System.Windows.Forms.DateTimePicker();
+            this.DTP2 = new System.Windows.Forms.DateTimePicker();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DTP3 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DTP1
+            // 
+            this.DTP1.Location = new System.Drawing.Point(44, 51);
+            this.DTP1.Name = "DTP1";
+            this.DTP1.Size = new System.Drawing.Size(200, 20);
+            this.DTP1.TabIndex = 0;
+            // 
+            // DTP2
+            // 
+            this.DTP2.Location = new System.Drawing.Point(44, 100);
+            this.DTP2.Name = "DTP2";
+            this.DTP2.Size = new System.Drawing.Size(200, 20);
+            this.DTP2.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(44, 139);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "sottrazzione";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Risultato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // DTP3
+            // 
+            this.DTP3.Location = new System.Drawing.Point(381, 51);
+            this.DTP3.Name = "DTP3";
+            this.DTP3.Size = new System.Drawing.Size(200, 20);
+            this.DTP3.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(381, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "somma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "sottrazzione";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(530, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DTP3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.DTP2);
+            this.Controls.Add(this.DTP1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker DTP1;
+        private System.Windows.Forms.DateTimePicker DTP2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DTP3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cakcikatire du date winform/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cakcikatire_du_date_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            DateTime data1 = DTP1.Value;
+            DateTime data2 = DTP2.Value;
+            TimeSpan data3 = data2 - data1;
+            label1.Text = data3.Days.ToString() + " giorni";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime data1 = DTP3.Value;
+            string data2 = textBox1.Text.ToString();
+            if (int.TryParse(data2, out int data3))
+            {
+                DateTime data4 = data1.AddDays(data3);
+                label3.Text = data4.ToString("dd/MM/yyyy");
+            }
+            else
+            {
+                label3.Text = "Giorno non valido";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime data1 = DTP3.Value;
+            string data2 = textBox1.Text.ToString();
+            if (int.TryParse(data2, out int data3))
+            {               
+                DateTime data4 = data1.AddDays(-data3);
+                label3.Text = data4.ToString("dd/MM/yyyy");
+            }
+            else
+            {
+                label3.Text = "Giorno non valido";
+            }
+        }
+    }
+}
